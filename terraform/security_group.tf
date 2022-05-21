76c40125236170282e79751f457f4067f2ce45a6
moutazmuhammad <85679864+moutazmuhammad@users.noreply.github.com>
2022-05-22 01:35:12 +0200

    Update security_group.tf

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "private_access" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [module.network.vpc_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 
