2ba5f5129fbfda848add28eb819d50efc6ed73e1
moutazmuhammad <moutazmuhammad1997@gmail.com>
2022-05-21 21:34:08 +0200

    update

--- edit_inventory.sh
+++ edit_inventory.sh
@@ -1,2 +1,2 @@
 
-sed -i "s#.*ansible_host.*#server-a ansible_host= $(terraform -chdir=./terraform  output -raw private_Instace_IP) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/privateKey.pem#" /var/jenkins_home/workspace/terraform_apply/ansible/inventory
+sed -i "s#.*ansible_host.*#server-a ansible_host=$(terraform -chdir=./terraform  output -raw private_Instace_IP) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/privateKey.pem#" /var/jenkins_home/workspace/terraform_apply/ansible/inventory
